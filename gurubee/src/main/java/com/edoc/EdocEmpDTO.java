ba03d64cad7ac8a96ca085f7a618320933caf841
A <A@DESKTOP-JEONG>
2022-10-23 14:31:18 +0900

    10-23 sessionInfo 참고용

--- gurubee/src/main/java/com/edoc/EdocEmpDTO.java
+++ gurubee/src/main/java/com/edoc/EdocEmpDTO.java
@@ -4,7 +4,10 @@ public class EdocEmpDTO {
 	// 결재자
 	
 	private String id_apper; // 결재자 사번
-	private String pos_code; // 직급코드
+	private String name_apper; // 결재자 이름
+	private String dep_name; // 결재자 부서
+	private String pos_name; // 결재자 직급명
+	private int pos_code; // 결재자 직급코드
 	
 	private String apper_num; // 결재자 번호
 	private String app_num; // 결재번호. (E_Approval - app_num)
@@ -19,10 +22,28 @@ public class EdocEmpDTO {
 	public void setId_apper(String id_apper) {
 		this.id_apper = id_apper;
 	}
-	public String getPos_code() {
+	public String getName_apper() {
+		return name_apper;
+	}
+	public void setName_apper(String name_apper) {
+		this.name_apper = name_apper;
+	}
+	public String getDep_name() {
+		return dep_name;
+	}
+	public void setDep_name(String dep_name) {
+		this.dep_name = dep_name;
+	}
+	public String getPos_name() {
+		return pos_name;
+	}
+	public void setPos_name(String pos_name) {
+		this.pos_name = pos_name;
+	}
+	public int getPos_code() {
 		return pos_code;
 	}
-	public void setPos_code(String pos_code) {
+	public void setPos_code(int pos_code) {
 		this.pos_code = pos_code;
 	}
 	public String getApper_num() {
---
+++ gurubee/src/main/java/com/edoc/EdocFormDTO.java
@@ -0,0 +1,29 @@
+package com.edoc;
+
+public class EdocFormDTO {
+	
+	private String app_doc; // 문서구분
+	private String doc_form; // 문서폼
+	private int form_num; // 문서폼번호
+	
+	public String getApp_doc() {
+		return app_doc;
+	}
+	public void setApp_doc(String app_doc) {
+		this.app_doc = app_doc;
+	}
+	public String getDoc_form() {
+		return doc_form;
+	}
+	public void setDoc_form(String doc_form) {
+		this.doc_form = doc_form;
+	}
+	public int getForm_num() {
+		return form_num;
+	}
+	public void setForm_num(int form_num) {
+		this.form_num = form_num;
+	}
+
+	
+}
